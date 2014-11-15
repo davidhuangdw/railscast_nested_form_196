5f8039ddabcc0f620b09feba4f01ebc4537fa470
DavidHdw <tossboydavid@sina.com>
2014-11-15 16:38:30 +0800

    ajax add/remove nested_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def link_to_add_fields(name, f, association)
+    obj = f.object.send(association).klass.new
+    id = obj.object_id
+    fields = f.simple_fields_for(association, obj, child_index:id) do |builder|
+      render(association.to_s.singularize, f:builder)
+    end
+    link_to name, '#',
+            class:'add_fields',
+            data:{id:id, fields:fields.gsub("\n","")}
+  end
 end
